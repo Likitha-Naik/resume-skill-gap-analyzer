11b1a5cd1c740c267bf9a3f2bbd049edf571a238
LIKITHA S R <150325831+Likitha-Naik@users.noreply.github.com>
2025-12-22 22:17:40 +0530

    Update init_db.py

--- init_db.py
+++ init_db.py
@@ -1,17 +1,17 @@
 import sqlite3
 
-conn = sqlite3.connect("database.db")
+conn = sqlite3.connect('database.db')
 cur = conn.cursor()
 
 cur.execute("""
 CREATE TABLE IF NOT EXISTS users (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
-    username TEXT UNIQUE,
-    password TEXT
+    username TEXT UNIQUE NOT NULL,
+    password TEXT NOT NULL
 )
 """)
 
 conn.commit()
 conn.close()
 
-print("✅ Database created successfully")
+print("✅ Database initialized successfully!")
